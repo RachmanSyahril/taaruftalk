6d7d02e4e59cf8fa6e1da7ab4ebf3a2589683d74
Syahril Rachman <rachman.syahril@gmail.com>
2023-08-17 11:25:28 +0700

    Updates

--- _nuxt/static/1692244044/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftaaruftalk\u002F_nuxt\u002Fstatic\u002F1692244044",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftaaruftalk\u002F",assetsPath:"\u002Ftaaruftalk\u002F_nuxt\u002F",cdnURL:a}}}}(null));
